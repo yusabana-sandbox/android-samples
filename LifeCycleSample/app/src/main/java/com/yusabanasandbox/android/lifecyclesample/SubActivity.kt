c3ce119c425f6487ffeb03396d2ddc679358cb12
Yuji Takaesu <taka.esu.user@gmail.com>
2019-10-01 10:30:03 +0900

    feat: logging to LifeCycleSample

--- LifeCycleSample/app/src/main/java/com/yusabanasandbox/android/lifecyclesample/SubActivity.kt
+++ LifeCycleSample/app/src/main/java/com/yusabanasandbox/android/lifecyclesample/SubActivity.kt
@@ -2,11 +2,49 @@ package com.yusabanasandbox.android.lifecyclesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class SubActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("LifeCycleSample", "Sub onCreate() called.")
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
+    }
+
+    public override fun onStart() {
+        Log.i("LifeCycleSample", "Sub onStart() called.")
+        super.onStart()
+    }
+
+    public override fun onRestart() {
+        Log.i("LifeCycleSample", "Sub onReStart() called.")
+        super.onRestart()
+    }
+
+    public override fun onResume() {
+        Log.i("LifeCycleSample", "Sub onResume() called.")
+        super.onResume()
+    }
+
+    public override fun onPause() {
+        Log.i("LifeCycleSample", "Sub onPause() called.")
+        super.onPause()
+    }
+
+    public override fun onStop() {
+        Log.i("LifeCycleSample", "Sub onStop() called.")
+        super.onStop()
+    }
+
+    public override fun onDestroy() {
+        Log.i("LifeCycleSample", "Sub onDestroy() called.")
+        super.onDestroy()
+    }
+
+    fun onButtonClick(view: View): Unit {
+        finish()
     }
 }
