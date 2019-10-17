a0be488c9c382d83a496ee3a6f0dd571cfcfabad
Yuji Takaesu <taka.esu.user@gmail.com>
2019-10-17 15:22:33 +0900

    feat: use my toolbar without original action bar

--- ToolbarSample/app/src/main/java/com/yusabanasandbox/android/toolbarsample/MainActivity.kt
+++ ToolbarSample/app/src/main/java/com/yusabanasandbox/android/toolbarsample/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.yusabanasandbox.android.toolbarsample
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        toolbar.setLogo(R.mipmap.ic_launcher)
+        toolbar.setTitle(R.string.toolbar_title)
+        toolbar.setTitleTextColor(Color.WHITE)
+        toolbar.setSubtitle(R.string.toolbar_subtitle)
+        toolbar.setSubtitleTextColor(Color.LTGRAY)
+        setSupportActionBar(toolbar)
+
     }
 }
